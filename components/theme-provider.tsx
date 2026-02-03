4cdb3da9512c4e96fe400a0ccf628eedd7cd9690
Jannsoic <jannicschmalle@gmail.com>
2026-02-03 09:05:20 +0100

    Add files via upload

---
+++ components/theme-provider.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { createContext, useContext, useState } from 'react'
+
+const ThemeContext = createContext<any>(null)
+
+export function ThemeProvider({ children }: any) {
+  const [theme, setTheme] = useState('dark')
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => useContext(ThemeContext)
